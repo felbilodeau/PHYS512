a4e530b5d07d9b60cdc64fb728b3101606ca5e4d
FuturePhelps <felbilodeau@gmail.com>
2021-11-05 15:06:05 -0400

    added run and whiten files

---
+++ ps6/run.py
@@ -0,0 +1,7 @@
+# This is the file which needs to be run to get the results.
+# The methods are all described in more detail in their
+# specific files
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os

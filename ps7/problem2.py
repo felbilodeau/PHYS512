96b3c83d388581f041be0041dbaa8486a0ff16fe
FuturePhelps <felbilodeau@gmail.com>
2021-12-03 23:57:56 -0500

    now calculates the potential instead of the charge

--- ps7/problem2.py
+++ ps7/problem2.py
@@ -6,36 +6,41 @@ import matplotlib.pyplot as plt
 G = np.loadtxt("potential.txt", float, '#', ';')
 G_FT = np.fft.fft(G)
 
-def laplace(rho, mask):
-    global G_FT
-    # Copy the charge matrix
-    temp = np.copy(rho)*0
+def laplace(V, mask):
+    # Copy the potential and set it to zero on the mask
+    temp = np.copy(V)
+    temp[mask] = 0
 
-    # Set the charge off the mask to 0
-    temp[mask] = rho[mask]
+    # Calculate the sum of the neighbours, and set it to 0 on the mask
+    total = sum_neighbors(temp)
+    total[mask] = 0
 
-    # Apply the convolution
-    output = np.fft.ifft(G_FT * np.fft.fft(temp))
-
-    # Return the potential
-    return np.real(output)
+    # Return the charge
+    return temp - total / 4
 
 def get_RHS(V, mask):
-    # Copy the potential matrix and set everything to 0
-    temp = np.copy(V) * 0
+    # Copy the potential and set it to 0 everywhere
+    temp = 0*V
 
     # Restore the values on the mask
     temp[mask] = V[mask]
 
-    # Since our RHS is the potential we can just return temp
-    return temp
+    # Calculate the sum of the neighbours on the on the mask
+    RHS = sum_neighbors(V) / 4
+
+    # Set the RHS on the mask to 0
+    RHS[mask] = 0
+    return RHS
 
 V = np.loadtxt("mask.txt", int, '#', ';')
 mask = np.where(V == 1)
 
 b = get_RHS(V, mask)
-V_new = conjugate_grad_solve(laplace, mask, b, 0*b, 1e-9, V.shape[0]*3)
-V_new[mask] = V[mask]
+V_raw = conjugate_grad_solve(laplace, mask, b, 0*b, 1e-9, V.shape[0]*3)
+V_new = np.copy(V_raw)
+V_new[mask] = b[mask]
+
+np.savetxt("potential_raw.txt", V_raw, '%.18e', ';')
 
 plt.imshow(V_new)
 plt.show()
